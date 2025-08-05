67602b638fcd03d18c9f4e57a2fa758639154a16
Ichiro.S <sey323_nm@yahoo.co.jp>
2025-08-05 15:25:30 +0900

    :sparkles: チュートリアルのコードを実装

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from open_ai_agent_sample.main import main
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
